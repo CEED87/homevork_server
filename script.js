cc5ba2079042570afd16c6bcfc788255c812faef
CEED87 <ceed210787@gmail.com>
2021-07-24 11:41:29 +0300

    scroll

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
 const RANDOM_API = 'https://randomuser.me/api/';
-const arr = [];
 const details = document.querySelector('.card');
-
 
 const getUser = async (url) => {
     const res = await fetch(url);
     return res.json();
-}
+};
 
 const generateInfo = async () => {
     const data = await getUser(RANDOM_API);
     let userName = `${data.results[0].name.first} ${data.results[0].name.last}`;
     let id = data.results[0].id.name;
     let picture = data.results[0].picture.large;
-    const obj = {
-        userName,
-         id,
-         picture
-     };
-    arr.push(obj);
+    details.innerHTML += `
+    <div class="details">
+        <div class="user_photo horizontal_center" id="${id}">
+            <img src="${picture}">
+        </div>
+        <p id="user_title">Hi, My name is</p>
+        <p id="user_value" style="text-transform: capitalize;">${userName}</p>
+    </div>`; 
+};
+ function users() {
+    for (let i = 0; i < 12; i++) {
+         generateInfo();  
 }
-
-let generatePosts = async () => {
-    arr.forEach(el => {
-        details.innerHTML += `
-                                <div class="details">
-                                    <div class="user_photo horizontal_center" id="${el.id}">
-                                        <img src="${el.picture}">
-                                    </div>
-                                    <p id="user_title">Hi, My name is</p>
-                                    <p id="user_value" style="text-transform: capitalize;">${el.userName}</p>
-                                </div>`; 
-    });
 }
+users()
 
-let renderUsers = async () => {
-        for (let i = 0; i < 12; i++) {
-             await generateInfo();  
-        }
-        generatePosts();
-    };
-
-    renderUsers();
-
-    let ttt = async () => {
-        await generateInfo();
-        generatePosts();
-    }
-
-    document.addEventListener('scroll', checkPosition);
-         
-    
-   function checkPosition() {
-        
-        const height = document.body.offsetHeight;
-        const screenHeight = window.innerHeight;
+// document.addEventListener('scroll',  () => {
+//     if (window.scrollY + window.innerHeight > document.body.offsetHeight 
+//         && window.scrollY + window.innerHeight < document.body.offsetHeight + 10) {
+//         tttt();
      
-        const scrolled = window.scrollY;
+//         console.log('window.scrollY')
+        
+// //    console.log(window.innerHeight)
+// //    console.log(document.body.offsetHeight)
+   
+//     }
+    
+// })
+ 
+function checkPosition() {
+        
+    const height = document.body.offsetHeight;
+    const screenHeight = window.innerHeight;
+     
+    const scrolled = window.scrollY;
       
-        const threshold = height - screenHeight / 8;
+    const threshold = height - screenHeight / 2;
       
-        const position = scrolled + screenHeight;
-      
-        if (position >= threshold) {
-            ttt();
-          console.log('Обращение к серверу')
-        }
-      }
-      
-              
+    const position = scrolled + screenHeight;
+     
+    if (position >= threshold && position <= threshold + 80) {
+       
+            users();    
 
+       
+       console.log('вызов функции')
+    }
+    console.log(scrolled)
+    console.log(height)
+    //  console.log(window.scrollY)
+}
+window.addEventListener('scroll',checkPosition)
+// checkPosition();
+      
+
+
+ 
+  
